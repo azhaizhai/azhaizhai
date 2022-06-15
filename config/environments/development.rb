3625011da42212603c7d4c5f7b08c6bdf2863e9e
jay yang <jay.y@shopperplus.com>
2022-06-15 17:20:30 +0800

    fix memcached bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store, "localhost:11211", { namespace: "azhaizhai", expires_in: 1.weeks, compress: true, pool_size: 4}
+    config.cache_store = :memory_store, "localhost:11211", { namespace: "azhaizhai", expires_in: 1.weeks, compress: true }
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
